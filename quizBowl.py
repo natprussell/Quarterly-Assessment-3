e0c5245fd5a4fb271b3049d6be0a026eba800771
Natalie Russell <natprussell@gmail.com>
2024-04-03 17:47:17 -0500

    added correct answer when incorrect is choosen

--- quizBowl.py
+++ quizBowl.py
@@ -6,10 +6,9 @@ class Quiz:
     def __init__(self, root, subject):
         self.root = root
         self.root.title("Quiz")
-        self.current_question_index = 0
         self.subject = subject
         self.questions = self.load_questions_from_database()
-        self.display_question()
+        self.display_questions()
 
     def load_questions_from_database(self):
         connection = sqlite3.connect('QuizBowl.db')
@@ -19,54 +18,38 @@ class Quiz:
         connection.close()
         return questions
 
-    def display_question(self):
-        if self.current_question_index < len(self.questions):
-            question_data = self.questions[self.current_question_index]
+    def display_questions(self):
+        for question_data in self.questions:
             question_text = question_data[0]
             options = question_data[1:5]
             correct_answer = question_data[5]
-            self.question_answer = QuestionAnswer(self.root, question_text, options, correct_answer, self.next_question)
-        else:
-            self.show_result()
-
-    def next_question(self):
-        self.current_question_index += 1
-        self.display_question()
-
-    def show_result(self):
-        result_window = Tk()
-        result_label = Label(result_window, text="Quiz Finished!")
-        result_label.pack()
+            QuestionAnswer(self.root, question_text, options, correct_answer)
 
 class QuestionAnswer:
-    def __init__(self, master, strQuestion, listAnswers, correctAnswer, callback):
+    def __init__(self, master, strQuestion, listAnswers, correctAnswer):
         self.correctAnswer = correctAnswer
-        self.callback = callback 
         self.frameQA = ttk.Frame(master, relief="raised", padding=(5, 5))
         self.frameQA.pack()
         self.label1 = ttk.Label(self.frameQA, text=strQuestion)
         self.label1.pack()
         self.answerOptions = ttk.Combobox(self.frameQA, values=listAnswers)
         self.answerOptions.pack()
-        self.testButton = ttk.Button(self.frameQA, text="Submit", command=self.checkAnswer)
-        self.testButton.pack()
         self.labelFeedback = ttk.Label(self.frameQA)
         self.labelFeedback.pack()
+        self.testButton = ttk.Button(self.frameQA, text="Submit", command=self.checkAnswer)
+        self.testButton.pack()
 
     def checkAnswer(self):
         if self.answerOptions.get() == self.correctAnswer:
             self.labelFeedback.config(text="Correct!", foreground="green")
         else:
-            self.labelFeedback.config(text="Incorrect!", foreground="red")
-
-        self.callback()
+            self.labelFeedback.config(text=f"Incorrect! The correct answer is: {self.correctAnswer}", foreground="red")
 
 class SelectSubject:
     def __init__(self, root):
         self.root = root
         self.root.title("Pick a Subject")
         self.button_to_quiz = ttk.Button(self.root, text="Go to Quiz", command=self.open_quiz)
-        self.button_to_quiz.pack(pady=10)
         
     def open_quiz(self):
         subject = self.subject_combobox.get()
@@ -83,7 +66,7 @@ class SelectSubject:
         connection.close()
         return subjects
     
-    def setup_ui(self):
+    def select_subject_window(self):
         self.subjects = self.load_subjects()
         self.subject_combobox = ttk.Combobox(self.root, values=self.subjects)
         self.subject_combobox.pack(pady=10)
@@ -91,5 +74,5 @@ class SelectSubject:
 
 root = Tk()
 app = SelectSubject(root)
-app.setup_ui()
+app.select_subject_window()
 root.mainloop()
